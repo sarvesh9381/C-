5c2bb33c54fd25302b9a3340aa92d748ba04f1d5
Sarvesh Singh Adhikari <sarveshsingh9381@gmail.com>
2025-01-10 16:00:47 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+// formula - 0,1,1,2,3,5,8,13,21...........
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+
+    for(int i=1;i<=n;i++){
+        int nxtnum = a+b;
+        cout<<nxtnum<<" ";
+        a= b;
+        b=nxtnum;
+    }
+}
